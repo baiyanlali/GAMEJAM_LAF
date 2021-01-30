ef6113872a3c6766877c881aac7088a6fe3001b9
baiyanlali <zao44457660@126.com>
2021-01-30 14:28:54 +0800

    修复少量bug
    Player的null reference

--- Scripts/Models/PlatformerModel.cs
+++ Scripts/Models/PlatformerModel.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 [System.Serializable]
 public class PlatformerModel
 {
-    public PlayerControl player;
+    private PlayerControl _player;
+    public PlayerControl player {
+
+        get {
+            if (_player == null)
+            {
+                _player = GameObject.Find("Player").GetComponent<PlayerControl>();
+            }
+            return _player;
+        }
+        
+    }
     public Transform spawnPoint;
     public CameraController cameraController;
 }
